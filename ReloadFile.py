0f5179b7a80ddcbca8f856047875569a659bedc3
Francois Kruger <francois.kruger@triggerfish.co.za>
2020-06-25 11:39:15 +0200

    Updated the reload file

--- ReloadFile.py
+++ ReloadFile.py
@@ -4,7 +4,5 @@ from maya import cmds  # pylint: disable=import-error
 def run():
     # Get the current file path
     thisFilePath = cmds.file(q=True, sn=True)
-    # Create a copy of the file (to throw away unsaved changes)
-    cmds.file(new=True, force=True)
     # Load the old file back in
-    cmds.file(thisFilePath, open=True)
+    cmds.file(thisFilePath, force=True, open=True)
